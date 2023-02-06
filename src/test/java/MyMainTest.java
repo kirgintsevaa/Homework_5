f2e5ff5ce04e32d62a1a22f919847ab2d2890f69
kirgintsevaa <kirgintseva@tutu.tech>
2023-02-06 20:36:53 +0300

    new commit 3

--- src/test/java/MyMainTest.java
+++ src/test/java/MyMainTest.java
@@ -24,6 +24,7 @@ public class MyMainTest {
     public static void setUp() {
         WebDriverManager.chromedriver().setup();
     }
+
     @BeforeEach
     public void init() {
         ChromeOptions options = new ChromeOptions();
